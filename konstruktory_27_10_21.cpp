5b0dfcf1c267bc9ea247dd3bade38b774d88b7a3
Krynickii <90185118+Krynickii@users.noreply.github.com>
2021-10-27 07:52:41 +0200

    Add files via upload

---
+++ konstruktory_27_10_21.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Worker{
+	public:
+		int id{8};
+		string name{"Katarzyna"};
+		string surname{"Nowak"};
+		
+		Worker(int pId=1, string pName="Name", string pSurname="Surname");
+		
+		void getData();
+};
+
+Worker::Worker(int pId, string pName, string pSurname){
+	id=pId;
+	name=pName;
+	surname=pSurname;
+}
+
+void Worker::getData(){
+	cout<<"Id: "<<id<<"\Imiê: "<<name<<", nazwisko: "<<surname<<endl;
+}
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE,"polish");
+	
+	Worker nowak = Worker();
+	nowak.getData();
+	
+	Worker nowak1 = Worker(10, "Krystyna", "Pawlak");
+	nowak1.getData();
+	return 0;
+}
