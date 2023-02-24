56982c5752b6d184e9cc3b97c0707eb78d358d4d
Krynickii <90185118+Krynickii@users.noreply.github.com>
2023-02-24 08:41:48 +0100

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/csvReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class csvReader
+    {
+        private string csvFilePath = "";
+
+        public csvReader(string _csvFilePath)
+        {
+            csvFilePath = _csvFilePath;
+        }
+
+        public City[] ReadFirstNCities(uint nCities)
+        {
+
+        }
+    }
+}
